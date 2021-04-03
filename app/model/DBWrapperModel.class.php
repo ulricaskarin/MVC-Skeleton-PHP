1b3d1bf91a3c07f1d068291df3c462e61729d356
ulricaskarin <ulrica.skarin@hotmail.se>
2021-04-03 12:26:54 +0200

    Add Database Logic

---
+++ app/model/DBWrapperModel.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * ★ DBWrapperModel Class ★
+ *
+ * Creates new instance of the DatabaseModel Class.
+ *
+ * An abstract class that creates a new instance of the Database.
+ * Allows interaction with the database without creating a new
+ * instance in each class.
+ *
+ * All other classes in \model extends this class for example:
+ *
+ * Class UserModel extends \model\DBWrapperModel
+ *
+ * @author: ulricaskarin
+ * @version 1.0.0
+ */
+
+ namespace models;
+
+ abstract class DBWrapperModel
+ {
+   protected $db;
+
+   public function __construct()
+   {
+     $this->db = new DatabaseModel();
+   }
+ }
---
+++ app/model/DatabaseModel.class.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * ★ DatabaseModel Class ★
+ *
+ * Connects to database and simplifies PDO functions.
+ *
+ * The Database will be initialized with the credentials
+ * of the SQL database, and will create a new PDO instance.
+ *
+ * The credentials are stored in _config/_db_config.php for example:
+ * define('DB_HOST', 'localhost');
+ * define('DB_USER', 'user');
+ * define('DB_PASS', 'myPass');
+ * define('DB_NAME', 'myDatabase');
+ *
+ * The rest of the model classes will access it by extending the
+ * DBWrapperModel class, as $this->db.
+ *
+ * @author: ulricaskarin
+ * @version 1.0.0
+ */
+
+ namespace models;
+
+ // ★ Requires credentials:
+ require_once(ROOT_PATH.'/app/_config/db_config.php');
+
+ use PDO;
+
+ class DatabaseModel
+{
+
+  /**
+  * Private properties for database access
+  * using secretly defined constants
+  */
+  private $host = DB_HOST;
+  private $user = DB_USER;
+  private $pass = DB_PASS;
+  private $dbname = DB_NAME;
+
+  /**
+  * Handler -> the PDO instance.
+  * Representing the connection to the database.
+  * @var PDO
+  */
+  private $handler;
+
+  private $error;
+
+  private $statement;
+
+  /**
+  * Initialize PDO connection. Set the handler as
+  * the new instance to be used throughout each additional
+  * methods.
+  *
+  * @throws PDOException - if failing to create PDO object.
+      */
+  public function __construct()
+  {
+   $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+   $options = [
+    PDO::ATTR_PERSISTENT => true,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+   ];
+
+   try {
+     $this->handler = new PDO($dsn, $this->user, $this->pass, $options);
+   } catch (PDOException $e) {
+     $this->error = $e->getMessage();
+   }
+  }
+
+  /**
+  * Prepares the statement as protection against SQL-injections.
+  * @param  string $query
+  */
+  public function query(string $query)
+  {
+    $this->statement = $this->handler->prepare($query);
+  }
+
+  /**
+   * Bind the variables to the proper type.
+   * Allows for integer, string, null and boolean.
+   */
+  public function bind($param, $value, $type = null)
+  {
+    if (is_null($type)) {
+      switch (true) {
+        case is_int($value):
+        $type = PDO::PARAM_INT;
+        break;
+
+        case is_bool($value):
+        $type = PDO::PARAM_BOOL;
+        break;
+
+        case is_null($value):
+        $type = PDO::PARAM_NULL;
+        break;
+        default:
+        $type = PDO::PARAM_STR;
+      }
+    }
+    $this->statement->bindValue($param, $value, $type);
+  }
+
+  /**
+   * Executes the prepared statement.
+   *
+   * @throws PDOException - if failing to execute.
+   */
+  public function execute()
+  {
+    try {
+      return $this->statement->execute();
+    } catch (PDOException $e) {
+      $this->error = $e->getMessage();
+    }
+  }
+
+  /**
+   * Fetch a single row as a result of a query.
+   * @return array - associative array
+   */
+  public function result()
+  {
+    $this->execute();
+    return $this->statement->fetch(PDO::FETCH_ASSOC);
+  }
+
+  /**
+   * Fetch a set of rows as a result of a query.
+   * @return array - associative array
+   */
+  public function resultSet()
+  {
+    $this->execute();
+    return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  /**
+   * Get the row count of the statement.
+   */
+  public function rowCount()
+  {
+    return $this->statement->rowCount();
+  }
+
+  /**
+   * Get the id of the last inserted item into the database.
+   * @return [type] [description]
+   */
+  public function lastInsertId()
+  {
+    return $this->handler->lastInsertId();
+  }
+
+  /**
+   * Disconnects from the database.
+   */
+  public function disconnect()
+  {
+    $this->handler = null;
+  }
+}
